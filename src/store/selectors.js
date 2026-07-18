209910b3d38cd2b09519d16d5f42bcb066dfb430
Frotrest <125584696+frotrest@users.noreply.github.com>
2026-07-18 11:38:37 +0300

    Update selectors.js

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,9 +1,12 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 export const selectArticles = (state) => state.articles.articles;
 
 export const selectCurrentType = (state) => state.articles.currentType;
 export const selectLoading = (state) => state.articles.loading;
 
-export const selectArticleById = (state, id) =>
-  state.articles.articles.find((article) => article.article_id === id);
+export const selectArticleById = createSelector([selectArticles, selectArticleId], (articles, id) => {
+  return articles.find((article) => article.article_id === id);
+})
 
 export const selectFavorites = (state) => state.favorites.favorites;
